42a16491905751bfd8f7122fa5a1fe32d2578516
Marcel <marcelb@uvic.ca>
2019-03-02 15:21:48 -0800

    fix indentation error

--- app/collisions.py
+++ app/collisions.py
@@ -40,7 +40,7 @@ def avoid_walls(data, weight):
     return criteria
 
 def avoid_almost_walls(data, weight):
-       criteria = {"goal": "avoid_almost_walls", "weight": weight}
+    criteria = {"goal": "avoid_almost_walls", "weight": weight}
     # walls: places with x, y outside the game area
 
     # where we are
@@ -52,7 +52,7 @@ def avoid_almost_walls(data, weight):
     # possible directions we can move
     directions = [1.0, 1.0, 1.0, 1.0]
 
-    if our_head["x"] + 1 >= (board["width"] - 1):
+    if our_head["x"] + 1 >= board["width"] - 1:
         directions[RIGHT] = 1
     else:
         directions[RIGHT] = 20
@@ -62,7 +62,7 @@ def avoid_almost_walls(data, weight):
     else:
         directions[LEFT] = 20
 
-    if our_head["y"] + 1 >= (board["height"] - 1):
+    if our_head["y"] + 1 >= board["height"] - 1:
         directions[DOWN] = 1
     else:
         directions[DOWN] = 20
